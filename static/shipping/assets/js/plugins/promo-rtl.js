954a4cc413b1e3cb1dd60170fdaf7e17bcb53d4d
aaj <mando.alvarado.jose@gmail.com>
2016-08-31 08:19:06 -0600

    Removed Shipping theme

--- static/shipping/assets/js/plugins/promo-rtl.js
+++
@@ -1,3 +0,0 @@
-$(function() {
-	$(".fullscreen-static-image").backstretch(["../assets/img-temp/PeopleImages.com-312380-zoom.jpg"],{duration: 5000, fade:1500});
-});
--- static/shipping/assets/js/plugins/promo.js
+++
@@ -1,3 +0,0 @@
-$(function() {
-	$(".fullscreen-static-image").backstretch(["/static/shipping/assets/img/landing_bg.jpg"],{duration: 5000, fade:1500});
-});
