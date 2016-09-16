2b95754baa5afff614a259680c96e5663f92acbe
quantcompany <director.qc@gmail.com>
2016-09-16 11:27:20 -0500

    DB settings

--- PHRI/settings.py
+++ PHRI/settings.py
@@ -84,8 +84,12 @@ WSGI_APPLICATION = 'PHRI.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'phri',
+        'USER': 'phri_user',
+        'PASSWORD': 'DB_phri_16092016',
+        'HOST': '',
+        'PORT': '',
     }
 }
 
