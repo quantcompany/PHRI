5443e49e20cacd679fcd72d76477434106fce4b4
aaj <mando.alvarado.jose@gmail.com>
2016-09-19 08:33:21 -0600

    oops

--- PHRI/settings.py
+++ PHRI/settings.py
@@ -132,7 +132,7 @@ USE_TZ = True
 
 # Static files
 STATIC_URL = '/static/'
-MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static')
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 
