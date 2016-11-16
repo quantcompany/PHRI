5a293b41ba01887449e50f14aa38c3c7cdadb475
aaj <mando.alvarado.jose@gmail.com>
2016-11-16 09:27:52 -0600

    put secret_key in .env

--- PHRI/settings.py
+++ PHRI/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '7(1o23j!nbss!$80_=8brvjy2o%d05!3xok93h7u)%os7h4gsx'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
