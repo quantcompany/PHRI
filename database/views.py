5f8aa594dd9efc2787432fd207d2a8e8e5154485
Armando <mando.alvarado.jose@gmail.com>
2017-03-09 23:32:00 -0600

    Added 'Type of Atrial Fibrillation' to medical report. Restricted access to database page and database dump download. Removed 'Chosen Therapy' section from patient report. Fixed password change form in the admin.

--- database/views.py
+++ database/views.py
@@ -12,14 +12,16 @@ from data_entry.models import Patient
 
 @login_required
 def index(request):
+    if not request.user.researcher:
+        raise PermissionDenied
     context = {'csv_size_estimate': patients_csv_size_estimate()}
     return render(request, 'database/index.html', context)
 
 
 @login_required
 def dump(request):
-    # if not request.user.researcher:
-    #     raise PermissionDenied
+    if not request.user.researcher:
+        raise PermissionDenied
     filename = 'patient_dump_{0}.csv'.format(timezone.now().strftime('%Y%m%d'))
     resp = StreamingHttpResponse(patients_csv(), content_type='text/csv; charset=utf-8')
     resp['Content-Disposition'] = 'attachment; filename="{0}"'.format(filename)
