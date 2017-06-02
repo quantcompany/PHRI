6cd129195b663e1ff7d2452d0f18fc4175d7baf4
wilguevara <wilrodriguezguevara@gmail.com>
2017-06-02 09:56:59 -0600

    validaciones y data binding de las propiedades creatinine y hemoglobin

--- data_entry/static/data_entry/js/scores.js
+++ data_entry/static/data_entry/js/scores.js
@@ -290,7 +290,6 @@ $('#creatinine_measure').on('change', function(){
         $('label[for^="creatinine_'+measure+'"]').stop(true, true).show(0);
     });
 });
-
 $('#hemoglobin_measure').on('change', function(){
     var measure = $(this).val();
     $('input[name^="hemoglobin_"]')
@@ -303,48 +302,44 @@ $('#hemoglobin_measure').on('change', function(){
     });
 });
 
+/**
+    Compute the transformation of values over the measure based on the input the user is using
+    For: Hemoglobin & Creatinine
+**/
 $('#hemoglobin_mgdL').on('keyup', function(){
-    var _value = $(this).val();
+    var _value = parseFloat($(this).val(), 10);
     if( isNaN(_value) ) {
-        console.log('isNaN');
-        $('#hemoglobin_gL').setAttr('value','');
+        $('#hemoglobin_gL').attr('value','');
         return;
     }
-    console.log('Is not isNaN');
-    var calc = (parseFloat(_value, 10) || 0 ) * 0.01;
+    var calc = _value * 0.01;
     $('#hemoglobin_gL').val( calc.toFixed(2) );
 });
 $('#hemoglobin_gL').on('keyup', function(){
-    var _value = $(this).val();
+    var _value = parseFloat($(this).val(), 10);
     if( isNaN(_value) ) {
-        console.log('isNaN');
-        $('#hemoglobin_mgdL').setAttr('value','');
+        $('#hemoglobin_mgdL').attr('value','');
         return;
     }
-    console.log('Is not isNaN');
-    var calc = (parseFloat(_value(), 10) || 0 ) / 0.01;
+    var calc = _value / 0.01;
     $('#hemoglobin_mgdL').val( calc.toFixed(2) );
 });
 
 $('#creatinine_mgdL').on('keyup', function(){
-    var _value = $(this).val();
+    var _value = parseFloat($(this).val(), 10);
     if( isNaN(_value) ) {
-        console.log('isNaN');
-        $('#creatinine_umolL').setAttr('value','');
+        $('#creatinine_umolL').attr('value','');
         return;
     }
-    console.log('Is not isNaN');
-    var calc = (parseFloat(_value, 10) || 0 ) * 88.5;
+    var calc = _value * 88.5;
     $('#creatinine_umolL').val( calc.toFixed(2) );
 });
 $('#creatinine_umolL').on('keyup', function(){
-    var _value = $(this).val();
+    var _value = parseFloat($(this).val(), 10);
     if( isNaN(_value) ) {
-        console.log('isNaN');
-        $('#creatinine_mgdL').setAttr('value','');
+        $('#creatinine_mgdL').attr('value','');
         return;
     }
-    console.log('Is not isNaN');
-    var calc = (parseFloat(_value, 10) || 0 ) / 88.5;
+    var calc = _value / 88.5;
     $('#creatinine_mgdL').val( calc.toFixed(2) );
 });
